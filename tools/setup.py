1f97a4b932142362e6be727e6d2def21eca222cf
codythecoder <cody.lovett.70@gmail.com>
2018-03-01 19:18:06 +1100

    Making the networks bigger
    didn't fix things but why not

--- tools/setup.py
+++ tools/setup.py
@@ -11,8 +11,8 @@ nn_outputs = 1
 input_shape = (-1, scan_size, scan_size, color_channels)
 input_size = color_channels*(scan_size**2)
 
-CNN_filter_sizes = (5, 3, 3)
-CNN_features = (9, 16, 25)
-CNN_strides = (2, 2, 2)
+CNN_filter_sizes = (7, 5, 3, 3, 3)
+CNN_features = (9, 16, 25, 32, 32)
+CNN_strides = (2, 2, 1, 1, 1)
 
-FCL_sizes = (3136, 128, nn_outputs)
+FCL_sizes = (23814, 1024, 512, 128, nn_outputs)
